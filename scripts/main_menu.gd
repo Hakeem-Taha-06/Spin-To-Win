bb38a7de18e968a05c4e44c93295c1adf147698a
Hakeem-Taha-06 <hakeem.taha06@eng-st.cu.edu.eg>
2026-06-24 14:26:48 +0300

    remove speed slider from main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @onready var weapon_list = $VBoxContainer/WeaponList
-@onready var speed_slider = $VBoxContainer/SpeedSlider
 @onready var start_button = $VBoxContainer/StartButton
 @onready var speed_value = $SpeedValue
 
@@ -12,10 +11,6 @@ func _ready() -> void:
 	weapon_list.add_item("Axe")
 	weapon_list.add_item("Spear")
 	
-	speed_slider.min_value = 100.0
-	speed_slider.max_value = 400.0
-	speed_slider.value = 200.0
-	
 	start_button.pressed.connect(_on_start_button_pressed)
 	# find and select the saved weapon in the list
 	for i in weapon_list.item_count:
@@ -23,13 +18,8 @@ func _ready() -> void:
 			weapon_list.select(i)
 			break
 
-func _process(delta: float) -> void:
-	speed_value.text = "{0}".format([speed_slider.value])
-
 func _on_start_button_pressed():
 	var selected_weapon: String = weapon_list.get_item_text(weapon_list.selected)
-	var selected_speed: float = speed_slider.value
 	GameState.player_weapon = selected_weapon
-	GameState.player_speed = selected_speed
 	GameState.save_settings()
 	GameState.start_game()
